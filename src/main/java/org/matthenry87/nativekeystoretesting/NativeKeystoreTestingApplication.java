90b2186cb9ba7861a11faf7582cddec69e280190
Mått Hëñry <Matt.Henry@fisglobal.com>
2023-10-16 15:30:28 -0600

    Add code

--- src/main/java/org/matthenry87/nativekeystoretesting/NativeKeystoreTestingApplication.java
+++ src/main/java/org/matthenry87/nativekeystoretesting/NativeKeystoreTestingApplication.java
@@ -1,13 +1,70 @@
 package org.matthenry87.nativekeystoretesting;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
-@SpringBootApplication
 public class NativeKeystoreTestingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NativeKeystoreTestingApplication.class, args);
+
+		var partnerApiKeystorePassword = System.getenv("PARTNER_API_KEYSTORE_PASSWORD");
+
+		var keyService = new KeyService("/home/u463254/partnerAPIKeyStore", partnerApiKeystorePassword, "JCEKS");
+
+		var key = keyService.retrieveKey("commhub.jwt.key");
+
+		System.out.println("Done: " + key.toString());
+	}
+
+}
+
+class KeyService {
+
+	private final String keyStoreLocation;
+	private final String keyStorePassword;
+	private final String keyStoreType;
+
+	public KeyService(String keyStoreLocation, String keyStorePassword, String keyStoreType) {
+		this.keyStoreLocation = keyStoreLocation;
+		this.keyStorePassword = keyStorePassword;
+		this.keyStoreType = keyStoreType;
+	}
+
+	public Key retrieveKey(String keyName) {
+
+		try {
+
+			File file = new File(this.getClass().getClassLoader().getResource(keyStoreLocation).getFile());
+
+			try (FileInputStream fis = new FileInputStream(file)) {
+
+
+				KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+				keyStore.load(fis, keyStorePassword.toCharArray());
+
+				var key = keyStore.getKey(keyName, keyStorePassword.toCharArray());
+
+				System.out.println(key.getAlgorithm());
+
+				return key;
+			}
+		} catch (IOException | KeyStoreException | NoSuchAlgorithmException |
+				 CertificateException | UnrecoverableKeyException e) {
+
+			throw new KeyServiceException("Error retrieving key from keystore", e);
+		}
+	}
+
+	private static class KeyServiceException extends RuntimeException {
+
+		private KeyServiceException(String message, Exception e) {
+
+			super(message, e);
+		}
+
 	}
 
 }
