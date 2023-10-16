2b961885f2071e853c98dcd775f6d95530e03004
Mått Hëñry <Matt.Henry@fisglobal.com>
2023-10-16 17:40:03 -0600

    Print registered providers

--- src/main/java/org/matthenry87/nativekeystoretesting/NativeKeystoreTestingApplication.java
+++ src/main/java/org/matthenry87/nativekeystoretesting/NativeKeystoreTestingApplication.java
@@ -5,70 +5,74 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.util.Arrays;
 
 public class NativeKeystoreTestingApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		var partnerApiKeystorePassword = System.getenv("PARTNER_API_KEYSTORE_PASSWORD");
+        var partnerApiKeystorePassword = System.getenv("PARTNER_API_KEYSTORE_PASSWORD");
 
-		System.out.println(partnerApiKeystorePassword);
+//		var keyService = new KeyService("C:\\keystore\\partnerAPIKeyStore", partnerApiKeystorePassword, "JCEKS");
+        var keyService = new KeyService("/home/u463254/partnerAPIKeyStore", partnerApiKeystorePassword, "JCEKS");
 
-		var keyService = new KeyService("/home/u463254/partnerAPIKeyStore", partnerApiKeystorePassword, "JCEKS");
+        var key = keyService.retrieveKey("commhub.jwt.key");
 
-		var key = keyService.retrieveKey("commhub.jwt.key");
-
-		System.out.println("Done: " + key.toString());
-	}
+        System.out.println("Done: " + key.toString());
+    }
 
 }
 
 class KeyService {
 
-	private final String keyStoreLocation;
-	private final String keyStorePassword;
-	private final String keyStoreType;
+    private final String keyStoreLocation;
+    private final String keyStorePassword;
+    private final String keyStoreType;
 
-	public KeyService(String keyStoreLocation, String keyStorePassword, String keyStoreType) {
-		this.keyStoreLocation = keyStoreLocation;
-		this.keyStorePassword = keyStorePassword;
-		this.keyStoreType = keyStoreType;
-	}
+    public KeyService(String keyStoreLocation, String keyStorePassword, String keyStoreType) {
+        this.keyStoreLocation = keyStoreLocation;
+        this.keyStorePassword = keyStorePassword;
+        this.keyStoreType = keyStoreType;
+    }
 
-	public Key retrieveKey(String keyName) {
+    public Key retrieveKey(String keyName) {
 
-		try {
+        Arrays.stream(Security.getProviders())
+                .map(Provider::getClass)
+                .map(Class::getName)
+                .toList()
+                .forEach(System.out::println);
 
-			System.out.println(keyStoreLocation);
+        try {
 
-			File file = new File(keyStoreLocation);
+            File file = new File(keyStoreLocation);
 
-			try (FileInputStream fis = new FileInputStream(file)) {
+            try (FileInputStream fis = new FileInputStream(file)) {
 
 
-				KeyStore keyStore = KeyStore.getInstance(keyStoreType);
-				keyStore.load(fis, keyStorePassword.toCharArray());
+                KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+                keyStore.load(fis, keyStorePassword.toCharArray());
 
-				var key = keyStore.getKey(keyName, keyStorePassword.toCharArray());
+                var key = keyStore.getKey(keyName, keyStorePassword.toCharArray());
 
-				System.out.println(key.getAlgorithm());
+                System.out.println(key.getAlgorithm());
 
-				return key;
-			}
-		} catch (IOException | KeyStoreException | NoSuchAlgorithmException |
-				 CertificateException | UnrecoverableKeyException e) {
+                return key;
+            }
+        } catch (IOException | KeyStoreException | NoSuchAlgorithmException |
+                 CertificateException | UnrecoverableKeyException e) {
 
-			throw new KeyServiceException("Error retrieving key from keystore", e);
-		}
-	}
+            throw new KeyServiceException("Error retrieving key from keystore", e);
+        }
+    }
 
-	private static class KeyServiceException extends RuntimeException {
+    private static class KeyServiceException extends RuntimeException {
 
-		private KeyServiceException(String message, Exception e) {
+        private KeyServiceException(String message, Exception e) {
 
-			super(message, e);
-		}
+            super(message, e);
+        }
 
-	}
+    }
 
 }
