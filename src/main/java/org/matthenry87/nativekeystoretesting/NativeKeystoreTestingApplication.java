c1c730aa0a6f8d96d4309901a83638c7969d3642
Matt Henry <matt.henry@fisglobal.com>
2023-10-16 14:55:21 -0600

    first commit

---
+++ src/main/java/org/matthenry87/nativekeystoretesting/NativeKeystoreTestingApplication.java
@@ -0,0 +1,13 @@
+package org.matthenry87.nativekeystoretesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeKeystoreTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeKeystoreTestingApplication.class, args);
+	}
+
+}
